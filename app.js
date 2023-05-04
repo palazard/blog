9ad941824c4e1d2f070941fe956af152fe1043ea
palazard <alazard.pauline@gmail.com>
2023-05-04 16:33:34 +0800

    database implementation with mongoose in local

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 const express = require("express");
 // const ejs = require("ejs");
-var _ = require('lodash');
+const _ = require('lodash');
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,12 +12,35 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 app.set('view engine', 'ejs');
 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
+}
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.static("public"));
 
-const posts = [];
 
-app.get('/', (req, res) => res.render('home', {Content : homeStartingContent, posts}));
+const postSchema = new mongoose.Schema({
+  postTitle : {
+    type: String,
+    required: true
+  },
+  postContent: {
+    type: String,
+    required: true
+  }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+
+app.get('/', (req, res) => {
+  Post.find()
+   .then(posts => res.render('home', {Content : homeStartingContent, posts}))
+   .catch(err=>console.log(err));
+});
 
 app.get('/about', (req, res) => res.render('about', {Content : aboutContent}));
 
@@ -26,20 +50,18 @@ app.get('/compose', (req, res) => res.render('compose'));
 
 app.post('/compose', (req, res) => {
   const {postTitle, postContent} = req.body;
-  posts.push({
+  const newPost = new Post ({
     postTitle,
     postContent
   });
-
+  newPost.save();
   res.redirect('/');
 });
 
-app.get('/post/:postTitle', (req, res) => {
-  posts.forEach(post => {
-    if (_.lowerCase(post.postTitle) === _.lowerCase(req.params.postTitle)) {
-      res.render('post', {postTitle: post.postTitle, postContent:post.postContent})
-    }
-  })
+app.get('/post/:_id', (req, res) => {
+   Post.findOne({_id: req.params._id})
+   .then(post =>res.render('post', {postTitle: post.postTitle, postContent: post.postContent}))
+   .catch(err=>console.log(err));
 });
 
 app.listen(3000, function() {
